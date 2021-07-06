b57272db3bf0c4c85f60de8cc8ede0c44deeb563
Jan Buschtöns <jan@buschtoens.me>
2021-07-06 13:37:00 +0200

    fix: update `visibleResultsQuerySelector`

--- js/utils.js
+++ js/utils.js
@@ -32,7 +32,34 @@ var shortcuts = {
   inputElementIds: ['cwtltblr' /* Google Calculator Widget */],
   inputElementTypes: ['text', 'number', 'textarea'],
 
-  visibleResultsQuerySelector: 'h3 a, #search a[data-ved][ping]',
+  // Finds all currently visible search result links.
+  // Unfortunately Google does not clearly mark search result links with a
+  // unique selector, so this selector is a brittle heuristic that may break any
+  // time Google changes the DOM structure / selectors, requiring a patch update
+  // of the addon to be pushed.
+  // Perspectively it may be beneficial to make this selector (or all of them)
+  // user-configurable, to enable users to temporarily fix it themselves, until
+  // a new patch update is out. We may also want to investigate a different
+  // approach, that e.g. does not only rely on a query selector, but includes
+  // additional heuristics, to improve the resiliency.
+  visibleResultsQuerySelector:
+    // Select all links in the main search results container, including
+    // non-search-result noise, like web cache, feedback, and further UI.
+    '#search a' +
+    // This selection is further refined with these filters:
+    // All actual search results have a `data-ved` attribute, but not all links
+    // with that attribute are search results.
+    '[data-ved]' +
+    // Some of them are interactive UI elements, like web cache, feedback, etc.
+    // They can be identified by the lack of a real target `href`.
+    ':not([href="#"])' +
+    // Google started inlining related questions and their search results as an
+    // accordion-type UI in an optional "People also ask" section. All accordion
+    // items are collapsed by default and marked as `aria-hidden` in that state.
+    // This filter excludes links that are invisible, as they are nested in a
+    // hidden ancestor element.
+    ':not([aria-hidden="true"] a)',
+
   resultContainerQuerySelector: 'div.gs_r, div.g, li, td',
   navigationContainerQuerySelector: 'div[role="navigation"] table',
   navigationLinksAndSuggestedSearchesQuerySelector: 'div[role="navigation"] table a, #botstuff a',
